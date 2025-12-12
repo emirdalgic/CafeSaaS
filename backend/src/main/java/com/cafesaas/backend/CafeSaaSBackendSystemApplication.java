bf177635ad63f0ca18a467c1bf84385fe2eac647
emirdalgic <emirdalgic681@gmail.com>
2025-12-12 22:05:21 +0300

    başlangıç: backend için dockerı konfigire ettim

---
+++ backend/src/main/java/com/cafesaas/backend/CafeSaaSBackendSystemApplication.java
@@ -0,0 +1,14 @@
+package com.cafesaas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSaaSBackendSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeSaaSBackendSystemApplication.class, args);
+	}
+
+}
